31c818de18cf081ea28ae7a49b9dc55ea95db1b2
Krecik <dzawisza00@gmail.com>
2023-10-12 22:51:48 +0200

    Guitar Tuner - IMPROVE COMPASS

--- app/src/main/java/com/zawisza/guitar_app/fragments/GuitarPick/GuitarPickFragment.java
+++ app/src/main/java/com/zawisza/guitar_app/fragments/GuitarPick/GuitarPickFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.zawisza.guitar_app.R;
@@ -33,6 +34,8 @@ public class GuitarPickFragment extends Fragment{
     private TextView textView;
     private TextView textView2;
     private ImageView imageView;
+    private int[] compassUp = {0,0,0};
+    private int[] compassDown = {0,0,0};
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -40,6 +43,8 @@ public class GuitarPickFragment extends Fragment{
         Log.d(TAG,"Open GuitarPickFragment");
 
         View view = inflater.inflate(R.layout.fragment_guitarpick, container, false);
+        ImageView compassUpImage = view.findViewById(R.id.compasUp);
+        ImageView compassDownImage = view.findViewById(R.id.compasDown);
         textView = view.findViewById(R.id.textView2);
         textView2 = view.findViewById(R.id.textView3);
         imageView = view.findViewById(R.id.arrow_guitarpick);
@@ -72,16 +77,40 @@ public class GuitarPickFragment extends Fragment{
                         
                         if(freq > 60 && freq < 96.205){
                             imageView.setRotation(300 + realFreq * 2);
+                            if(realFreq == 0){
+                                compassUp[0] = 1;
+                                changeCompass(compassUpImage, compassUp, true);
+                            }
                         } else if (freq < 128.415) {
                             imageView.setRotation(240 + realFreq * 2);
+                            if(realFreq == 0){
+                                compassDown[0] = 1;
+                                changeCompass(compassDownImage, compassDown, false);
+                            }
                         } else if (freq < 171.415) {
                             imageView.setRotation(180 + realFreq * 2);
+                            if(realFreq == 0){
+                                compassDown[1] = 1;
+                                changeCompass(compassDownImage, compassDown, false);
+                            }
                         } else if (freq < 221.47) {
                             imageView.setRotation(120 + realFreq * 2);
+                            if(realFreq == 0){
+                                compassDown[2] = 1;
+                                changeCompass(compassDownImage, compassDown, false);
+                            }
                         } else if (freq < 288.285) {
                             imageView.setRotation(60 + realFreq *2 );
+                            if(realFreq == 0){
+                                compassUp[2] = 1;
+                                changeCompass(compassUpImage, compassUp, true);
+                            }
                         } else if (freq < 350) {
                             imageView.setRotation(realFreq *2);
+                            if(realFreq == 0){
+                                compassUp[1] = 1;
+                                changeCompass(compassUpImage, compassUp, true);
+                            }
                         }
 
                         textView.setText(pitch.name);
@@ -102,5 +131,70 @@ public class GuitarPickFragment extends Fragment{
 
 
         return view;
+    }
+
+    private void changeCompass(ImageView compass, int[] compassInt, boolean i){
+        if(i){
+            if(compassInt[0] == 1){
+                if(compassInt[1] == 1){
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass17));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass14));
+                    }
+                }else{
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass15));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass11));
+                    }
+                }
+            }else{
+                if(compassInt[1] == 1){
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass16));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass12));
+                    }
+                }else{
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass13));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass1));
+                    }
+                }
+            }
+        }else{
+            if(compassInt[0] == 1){
+                if(compassInt[1] == 1){
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass27));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass24));
+                    }
+                }else{
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass26));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass21));
+                    }
+                }
+            }else{
+                if(compassInt[1] == 1){
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass25));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass22));
+                    }
+                }else{
+                    if(compassInt[2] == 1){
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass23));
+                    }else{
+                        compass.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.compass2));
+                    }
+                }
+            }
+        }
+
     }
 }
